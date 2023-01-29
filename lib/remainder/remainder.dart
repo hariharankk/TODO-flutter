d7d3b4c32ba9a9b90f157a10968211b4fb089564
77777spades77 <spadessoftwares>
2023-01-29 13:51:18 -0800

    'commit'

--- lib/remainder/remainder.dart
+++ lib/remainder/remainder.dart
@@ -5,8 +5,6 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 class Notify {
 
 
-
-
   static Future<void> createNewNotification() async {
     bool isAllowed = await AwesomeNotifications().isNotificationAllowed();
     if (!isAllowed) return;
@@ -68,4 +66,82 @@ class Notify {
         schedule: NotificationCalendar.fromDate(
             date: DateTime.now().add(const Duration(seconds: 10))));
   }
+
+
+  static Future<void> retrieveScheduledNotifications() async {
+    final AwesomeNotifications awesomeNotifications = AwesomeNotifications();
+    List<NotificationModel> scheduledNotifications =
+    await awesomeNotifications.listScheduledNotifications();
+    print(scheduledNotifications);
+  }
 }
+
+
+/*import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:notification/models/notify.dart';
+
+void main() {
+  runApp(const MyApp());
+  AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelGroupKey: 'reminders',
+        channelKey: 'instant_notification',
+        channelName: 'Basic Instant Notification',
+        channelDescription:
+        'Notification channel that can trigger notification instantly.',
+        defaultColor: const Color(0xFF9D50DD),
+        ledColor: Colors.white,
+      ),
+      NotificationChannel(
+        channelGroupKey: 'reminders',
+        channelKey: 'scheduled_notification',
+        channelName: 'Scheduled Notification',
+        channelDescription:
+        'Notification channel that can trigger notification based on predefined time.',
+        defaultColor: const Color(0xFF9D50DD),
+        ledColor: Colors.white,
+      ),
+    ],
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () async {
+                  await Notify.instantNotify();
+                },
+                child: const Text("Instant Notification "),
+              ),
+              TextButton(
+                onPressed: () async {
+                  await Notify.scheduleNotification();
+                },
+                child: const Text("Schedule Notification "),
+              ),
+              TextButton(
+                onPressed: () async {
+                  await Notify.retrieveScheduledNotifications();
+                },
+                child: const Text("Retrieve Scheduled Notifications"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+ }
+*/ 
